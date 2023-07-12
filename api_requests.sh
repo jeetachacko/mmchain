253a47a32b03d8cd8717e6849d71790618f549ec
jac <jeetachacko@gmail.com>
2023-07-12 16:54:50 +0200

    Update api_requests.sh

--- api_requests.sh
+++ api_requests.sh
@@ -1,5 +1,5 @@
 #Create_contract takes as input recordingId - integer, userId - integer and contractType - integer
-curl --request POST  --url http://localhost:3000/invoke  --header 'content-type: application/x-www-form-urlencoded'   --data =   --data channelid=mychannel  --data chaincodeid=basic  --data function=Create_contract  --data args=123  --data args=1  --data args=1
+curl --request POST  --url http://localhost:3000/invoke  --header 'content-type: application/x-www-form-urlencoded'   --data =   --data channelid=mychannel  --data chaincodeid=basic  --data function=Create_contract  --data args=333  --data args=6  --data args=1
 
 #Get_contracts takes as input userId - integer
 curl --request GET  --url 'http://localhost:3000/query?channelid=mychannel&chaincodeid=basic&function=Get_contracts&args=3'
